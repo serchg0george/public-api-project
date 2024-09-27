f43af2773b4582dc1d17e4468bd029f1b6f791c6
serchg0george <serchg0george@gmail.com>
2024-09-27 12:49:29 +0300

    Removed unused update functionality

--- src/app/components/animal-table/animal-table.component.ts
+++ src/app/components/animal-table/animal-table.component.ts
@@ -54,38 +54,8 @@ export class AnimalTableComponent implements OnInit {
     this.router.navigate(['/add-animal']);
   }
 
-  // updateAnimal(): void {
-  //   if (this.selectedAnimal) {
-  //     const updatedAnimal = {
-  //       id: this.selectedAnimal.id,
-  //       name: this.animalForm.get('name')?.value,
-  //       species: this.animalForm.get('species')?.value,
-  //       age: this.animalForm.get('age')?.value,
-  //       cage: { cageNumber: this.animalForm.get('cage')?.value },
-  //       health: { status: this.animalForm.get('health')?.value }
-  //     } as Animal;
-
-  //     this.animalService.editAnimal(this.selectedAnimal.id, updatedAnimal).subscribe((response) => {
-  //       console.log('Animal updated:', response);
-  //       this.fetchAnimals(); 
-  //       this.selectedAnimal = null;
-  //       this.animalForm.reset();
-  //     });
-  //   }
-
-  // }
-
   editAnimal(animal: Animal): void {
     this.router.navigate(['/edit-animal', animal.id]);
-    // this.selectedAnimal = { ...animal };
-    // this.animalForm.patchValue({
-    //   name: animal.name,
-    //   species: animal.species,
-    //   age: animal.age,
-    //   cage: animal.cage.cageNumber,
-    //   health: animal.health.status
-    // });
-    // this.fetchAnimals();
   }
 
   deleteAnimal(id: number): void {
